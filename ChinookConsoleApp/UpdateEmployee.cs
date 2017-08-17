03c3679c472a4052ac7b8c49cc5e269d574a1da5
Heather Thacker <hhthacker@gmail.com>
2017-08-16 20:57:10 -0500

    update query

---
+++ ChinookConsoleApp/UpdateEmployee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ChinookConsoleApp
+{
+    class UpdateEmployee
+    {
+        public void Update()
+        {
+            Console.Clear();
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                var employeeListCommand = connection.CreateCommand();
+
+                employeeListCommand.CommandText = "select employeeid as Id, " +
+                                                  "firstname + ' ' + lastname as fullname " +
+                                                  "from Employee";
+
+
+                var employeeUpdateCommand = connection.CreateCommand();
+
+                employeeUpdateCommand.CommandText = "update Employee " +
+                                        "set firstname = @firstname, lastname = @lastname " +
+                                        "where employeeId = @id" +
+                                        "and firstname + ' ' + lastname as fullname = @fullname ";
+
+                try
+                {
+                    connection.Open();
+                    var reader = employeeListCommand.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]}.) {reader["FullName"]}");
+                    }
+
+                    Console.WriteLine("Select an Employee number.");
+                    Console.Write(">");
+                    var updateSelection = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+    }
+}
