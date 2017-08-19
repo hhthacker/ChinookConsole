0ce27c4ab22da2cf548a36c10c63b8414cb286db
Heather Thacker <hhthacker@gmail.com>
2017-08-19 13:54:13 -0500

    working

--- ChinookConsoleApp/Program.cs
+++ ChinookConsoleApp/Program.cs
@@ -19,14 +19,19 @@ namespace ChinookConsoleApp
                 Console.WriteLine("------------Please select from the following options---------");
                 Console.WriteLine("1. List Employees");
                 Console.WriteLine("2. Add an Employee");
+                Console.WriteLine("3. Update an Employee");
+                Console.WriteLine("4. Delete an Employee");
                 Console.WriteLine("9. Exit");
                 Console.WriteLine("");
                 Console.Write(">");
                 var selection = Console.ReadLine();
 
 
-                if (selection == "1") new ListEmployees().List();
+                if (selection == "1") new ListEmployees().List("Press enter to return to menu");
                 if (selection == "2") new AddEmployee().Add();
+                if (selection == "3") new UpdateEmployee().Update();
+                if (selection == "4") new DeleteEmployee().Delete();
+
                 if (selection == "9") break;
 
             }
